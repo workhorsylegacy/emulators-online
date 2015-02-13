b89e403f3ac59122fe71abc83fdb29f9d5e8fe47
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-02-12 23:48:31 -0800

    Added configuration for Nintendo 64 gamepad.

--- server/mupen64plus.py
+++ server/mupen64plus.py
@@ -32,12 +32,92 @@ import emu_runner
 import base_console
 
 
+BUTTON_CODE_MAP = {
+	'button_12' : '"hat(0 Up)"', # up
+	'button_13' : '"hat(0 Down)"', # down
+	'button_14' : '"hat(0 Left)"', # left
+	'button_15' : '"hat(0 Right)"', # right
+	'button_9' : '"button(7)"', # start
+	'button_0' : '"button(0)"', # A
+	'button_2' : '"button(2)"', # X
+	'button_4' : '"button(4)"', # L Shoulder button
+	'button_5' : '"button(5)"', # R Shoulder button
+	'button_7' : '"axis(2+)"', # R Trigger
+	'button_6' : '"axis(2-)"', # L Trigger
+	'axes_0-' : '"axis(0-,0+)"', # L Stick Left
+	'axes_0+' : '"axis(0-,0+)"', # L Stick Right
+	'axes_1-' : '"axis(1-,1+)"', # L Stick Up
+	'axes_1+' : '"axis(1-,1+)"', # L Stick Down
+	'axes_2-' : '"axis(4-)"', # R Stick Left
+	'axes_2+' : '"axis(4+)"', # R Stick Right
+	'axes_3-' : '"axis(3-)"', # R Stick Up
+	'axes_3+' : '"axis(3+)"', # R Stick Down
+	None : ''
+}
+
 class Mupen64Plus(base_console.BaseConsole):
 	def __init__(self):
 		super(Mupen64Plus, self).__init__('config/mupen64plus.json')
 
+		# Setup the initial map, if there is none
+		if not self.button_map:
+			self.button_map = {
+				'btnUpMupen' : None,
+				'btnDownMupen' : None,
+				'btnLeftMupen' : None,
+				'btnRightMupen' : None,
+				'btnStartMupen' : None,
+				'btnAMupen' : None,
+				'btnBMupen' : None,
+				'btnZMupen' : None,
+				'btnLShoulderMupen' : None,
+				'btnRShoulderMupen' : None,
+				'btnStickXMupen' : None,
+				'btnStickYMupen' : None,
+				'btnCUpMupen' : None,
+				'btnCDownMupen' : None,
+				'btnCLeftMupen' : None,
+				'btnCRightMupen' : None
+			}
+
+	def _setup_config(self):
+		config = {
+			'Input-SDL-Control1' : {
+				'version' : 2,
+				'device' : 0,
+				'mode' : 0,
+				'plugged' : True,
+				'plugin' : 2,
+				'mouse' : False,
+				'AnalogDeadzone' : '"4096,4096"',
+				'AnalogPeak' : '"32768,32768"',
+				'DPad R' : BUTTON_CODE_MAP[self.button_map['btnRightMupen']],
+				'DPad L' : BUTTON_CODE_MAP[self.button_map['btnLeftMupen']],
+				'DPad D' : BUTTON_CODE_MAP[self.button_map['btnDownMupen']],
+				'DPad U' : BUTTON_CODE_MAP[self.button_map['btnUpMupen']],
+				'Start' : BUTTON_CODE_MAP[self.button_map['btnStartMupen']],
+				'Z Trig' : BUTTON_CODE_MAP[self.button_map['btnZMupen']],
+				'B Button' : BUTTON_CODE_MAP[self.button_map['btnBMupen']],
+				'A Button' : BUTTON_CODE_MAP[self.button_map['btnAMupen']],
+				'C Button R' : BUTTON_CODE_MAP[self.button_map['btnCRightMupen']],
+				'C Button L' : BUTTON_CODE_MAP[self.button_map['btnCLeftMupen']],
+				'C Button D' : BUTTON_CODE_MAP[self.button_map['btnCDownMupen']],
+				'C Button U' : BUTTON_CODE_MAP[self.button_map['btnCUpMupen']],
+				'R Trig' : BUTTON_CODE_MAP[self.button_map['btnRShoulderMupen']],
+				'L Trig' : BUTTON_CODE_MAP[self.button_map['btnLShoulderMupen']],
+				'Mempak switch' : '""',
+				'Rumblepak switch' : '""',
+				'X Axis' : BUTTON_CODE_MAP[self.button_map['btnStickXMupen']],
+				'Y Axis' : BUTTON_CODE_MAP[self.button_map['btnStickYMupen']],
+				'name' : '"Custom pad"'
+			}
+		}
+
+		ini.write_ini_file(os.path.expanduser('~/AppData/Roaming/Mupen64Plus/mupen64plus.cfg'), config)
 
 	def run(self, path, binary):
+		self._setup_config()
+
 		# Run the game
 		os.chdir("emulators/Mupen64Plus/")
 		game_path = self.goodJoin("../../", path + '/' + binary)
